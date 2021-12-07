30cf4cdd9ae1d492f669da8f7cc1a6ba034c756a
Lara <larafpics@gmail.com>
2021-12-08 00:29:50 +0100

    chore: add properties to SiteItem class

--- SiteItem.cs
+++ SiteItem.cs
@@ -3,6 +3,10 @@ namespace PasswordAPI {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public string User { get; set; }
         public string Password { get; set; }
+        public string CreationDate { get; set; }
+        public string Description { get; set; }
+        public CategoryItem Category { get; set; }
     }
 }
