0108ca3ba582253c1c50064624014d83e6c23e58
Lara <larafpics@gmail.com>
2021-12-08 20:47:18 +0100

    chore: added HTTP methods for CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,11 +14,36 @@ namespace PasswordAPI.Controllers
     {
         public static List<CategoryItem> Categories = new List<CategoryItem> {
             new CategoryItem (1, "Redes Sociales"),
+            new CategoryItem (2, "Herramientas de trabajo"),
+            new CategoryItem (3, "Streaming"),
         };
 
         [HttpGet]
         public ActionResult<List<CategoryItem>> Get() {
             return Ok(Categories);
+        }
+
+        [HttpPost]
+        public ActionResult Post(CategoryItem categoryItem) {
+            var existingCategoryItem = Categories.Find(x => x.Id == categoryItem.Id);
+            if (existingCategoryItem != null) {
+                return Conflict("Ya existe una categoría con esa ID.");
+            } else {
+                Categories.Add(categoryItem);
+                var resourceUrl = Request.Path.ToString() + "/" + categoryItem.Id;
+                return Created(resourceUrl, categoryItem);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int Id) {
+            var categoryItem = Categories.Find(x => x.Id == Id);
+            if (categoryItem == null) {
+                return NotFound("No existe una categoría con esa ID.");
+            } else {
+                Categories.Remove(categoryItem);
+                return NoContent();
+            }
         }
     }
 }
